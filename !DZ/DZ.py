7d669c9a5cf271a7d65d433d252b3a79df58c832
Vedmid20 <lysyy.boba.freelance@gmail.com>
2024-02-17 23:30:58 +0200

    1

--- !DZ/DZ.py
+++ !DZ/DZ.py
@@ -1,102 +1,25 @@
-import flet as ft
+import tkinter
+
+root = tkinter.Tk()
+w = root.winfo_screenwidth() / 2 - 150
+h = root.winfo_screenheight() / 2 - 150
+root.geometry(f'300x300+{int(w)}+{int(h)}')
+root.wm_resizable(width=False,height=False)
+x = 0
+root.title('0')
+t = tkinter.Label(root, text='Hello world!', fg='sea green', font=(None, 30))
+t.pack(ipady=20)
+
+def change_text():
+    global t, x
+    x += 1
+    root.title(str(x))
+    if x % 2 == 0:
+        t.config(text='Hello world!')
+    else:
+        t.config(text='This is tkinter!')
 
 
-def main(page: ft.Page):
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.title = 'User Data'
-    page.window_width = 345
-    page.window_height = 470
-    page.window_resizable = False
-    page.theme_mode = 'dark'
-    t = ft.Text(value='Enter your data', size=30)
-    name = ft.TextField(label='Name', width=310, height=50)
-    year = ft.TextField(label='Year', width=150, height=50)
-    month = ft.TextField(label='Month', width=150, height=50)
-    successfullyTXT = ft.Text(value='', size=15, color='green')
-
-
-    def result():
-        if str(month.value) != '' and year.value != '' and name.value != '':
-            with open('file.txt', 'a', encoding='utf-8') as file:
-                file.write(f'Name: {name.value.title()}\nYear: {year.value}\nMonth: {month.value}\n-------------\n')
-                successfullyTXT.value = 'Successfully'
-        else:
-            pass
-
-    def validate_month():
-        try:
-            if int(month.value) <= 12 and int(month.value) >= 1:
-                month.border_color = 'green'
-                month.label = 'Month'
-                result()
-            else:
-                month.border_color = 'red'
-                month.label = 'Error'
-        except ValueError:
-            month.border_color = 'red'
-            month.label = 'Error'
-        page.update()
-
-    def validate_year():
-      try:
-          if  len(year.value) >= 1 and int(year.value) >= 5 and int(year.value) <= 120:
-              year.border_color = 'green'
-              year.label = 'Year'
-              validate_month()
-          else:
-              year.border_color = 'red'
-              year.label = 'Error'
-              validate_month()
-      except ValueError:
-          year.border_color = 'red'
-          year.label = 'Error'
-          validate_month()
-      page.update()
-
-    def validate(e):
-        try:
-            if len(str(name.value)) >= 2:
-                name.border_color = 'green'
-                name.label = 'Name'
-                validate_year()
-
-            else:
-                name.border_color = 'red'
-                name.label = 'Error'
-                validate_year()
-
-        except ValueError:
-            name.border_color = 'red'
-            name.label = 'Error'
-            validate_year()
-        page.update()
-
-    page.add(ft.Row([
-        t
-    ], alignment=ft.MainAxisAlignment.CENTER))
-    page.add(
-        ft.Row(
-            [
-                ft.Column([name])], alignment=ft.MainAxisAlignment.CENTER))
-
-    page.add(
-        ft.Row(
-            [
-                year,
-                month
-            ], alignment=ft.MainAxisAlignment.CENTER))
-    page.add(ft.Row([
-        successfullyTXT
-    ]))
-
-    page.add(
-        ft.Row(
-            [
-                ft.ElevatedButton(text='Accept', on_click=validate, width=140, height=40, color='white', bgcolor='grey800')
-            ], alignment=ft.MainAxisAlignment.CENTER))
-
-    page.update()
-
-
-if __name__ == '__main__':
-    ft.app(target=main)
+btn = tkinter.Button(root, text='Change text', background='brown2', borderwidth=3, command=change_text, relief='ridge', activebackground='firebrick3')
+btn.pack(ipadx = 10, ipady = 10)
+root.mainloop()
